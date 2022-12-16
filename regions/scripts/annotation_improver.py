4513a31af33199d40befca14cd00b6f77c1320bc
Adam English <ACEnglish@gmail.com>
2022-12-15 21:45:17 -0500

    anno improver finished
    maybe

--- regions/scripts/annotation_improver.py
+++ regions/scripts/annotation_improver.py
@@ -31,8 +31,11 @@ import math
 from functools import cmp_to_key
 
 import pysam
+import joblib
 import truvari
+import pandas as pd
 from intervaltree import IntervalTree
+
 
 def iter_tr_regions(fn, region=None):
     """
@@ -52,7 +55,19 @@ def iter_tr_regions(fn, region=None):
                "start": start,
                "end": end,
                "annos": annos}
-
+HEADER = ['chrom', 'start', 'end', 'ovl_flag', 'up_buff', 'dn_buff', 'n_filtered', 'n_annos', 'n_subregions',
+          'mu_purity', 'pct_annotated', 'interspersed', 'patho', 'codis', 'annos']
+def iter_tr_regions_1(fn):
+    """
+    Reads an already processed repeats file
+    """
+    data = truvari.opt_gz_open(fn)
+    for line in data:
+        line = {x:y for x,y in zip(HEADER, line.strip().split('\t'))}
+        line['annos'] = json.loads(line['annos'])
+        for i in HEADER[1:-4]:
+            line[i] = int(line[i])
+        yield line
 
 def same_intv(intv, tree):
     """
@@ -70,7 +85,11 @@ def get_subregions(annos):
     """
     i_tree = IntervalTree()
     for pos, anno in enumerate(annos):
-        anno['ovl_flag'] = 0
+        if 'ovl_flag' not in anno:
+            anno['ovl_flag'] = 0
+        if 'repeat' in anno:
+            anno['motif'] = anno['repeat']
+            del anno['repeat']
         i_tree.addi(anno['start'], anno['end'], pos)
     
     m_tree = i_tree.copy()
@@ -91,9 +110,7 @@ def anno_sorter(a1, a2):
         return 1
     if a1[0] < a2[0]:
         return -1
-    # if the boundaries are identical, I want the smaller motif?
-    #if a1[1]['start'] == a2[1]['start'] and a1[1]['end'] == a2[1]['end']:
-    #    return len(a2[1]['repeat']) 
+    # if the boundaries are identical, I want the higher score
     return a1[1]["score"] - a2[1]["score"]
 
 def make_span_sorted_list(annos):
@@ -125,9 +142,9 @@ def simplify_nested(annos):
     ret = []
     for l, anno in srt[:-1]:
         if len(m_tree.overlap(anno['start'], anno['end'])) == 1:
-            anno['ovl_flag'] |= 0x4
+            anno['ovl_flag'] |= 0x2 # nested
             ret.append(anno)
-    parent['ovl_flag'] |= 0x2
+    parent['ovl_flag'] |= 0x4 # parent
     ret.insert(0, parent)
     return ret
 
@@ -145,10 +162,10 @@ def viz_region(reg):
         mode = True
         for i in range(math.floor(anno['copies'])):
             if mode:
-                seq += anno['repeat']
+                seq += anno['repeat'] if 'repeat' in anno else anno['motif']
             else:
                 #seq += "[cyan]" + anno['repeat'].lower() + "[/]"
-                seq += anno['repeat'].lower()
+                seq += anno['repeat'].lower() if 'repeat' in anno else anno['motif'].lower()
             mode = not mode
     
         anno['end'] - anno['start']
@@ -167,6 +184,7 @@ def simplify_staggered(annos):
     max_span['ovl_flag'] |= 0x8
     keeps = [max_span]
     for l, intv in srt[:-1]:
+        intv['ovl_flag'] |= 0x8
         if max_span["start"] <= intv["start"] and intv["end"] <= max_span["end"]:
             keeps.append(intv)
         elif intv["start"] > max_span["end"] or intv["end"] < max_span["start"]:
@@ -209,9 +227,9 @@ def annotate_purity(reg):
         s_start = anno['start'] - reg['start']
         s_end = anno['end'] - reg['start']
         sub_seq = seq[s_start: s_end]
-        alt_seq = anno['repeat'] * int(math.floor(anno['copies']))
+        alt_seq = anno['motif'] * int(math.floor(anno['copies']))
         remain = int((anno['copies'] % 1) * len(anno))
-        alt_seq += anno['repeat'][:remain]
+        alt_seq += anno['motif'][:remain]
         if len(alt_seq) + len(sub_seq) == 0:
             print('what?')
             print(reg)
@@ -237,8 +255,7 @@ def pct_annotated(reg, start, end):
 
 def write_region(reg):
     out_str = []
-    for key in ['chrom', 'start', 'end', 'ovl_flag', 'up_buff', 'end_buff', 'n_filtered', \
-                'n_annos', 'n_subregions', 'mu_purity', 'pct_annotated']:
+    for key in HEADER[:-1]:
         out_str.append(str(reg[key]))
     out_str.append(json.dumps(reg['annos']))
     print("\t".join(out_str))
@@ -283,9 +300,65 @@ def get_buff(reg, f_start, l_end):
         dn_buff += 1
     return start, end, up_buff, dn_buff
 
+def get_interspersed(region, repeats):
+    """
+    Get the max scoring interspersed repeat annotation
+    """
+    key = f"{region['chrom']}:{region['start']}-{region['end']}"
+    ret = [_[1] for _ in sorted(repeats[key]) if _[1] not in ["Simple_repeat", "Low_complexity", "Satellite", "Unknown"]]
+    if ret:
+        return ret[-1]
+    return '.'
+
+class AnnoTree():
+    """
+    Simple annotation overlap helper
+    """
+    def __init__(self, fn):
+        self.tree, _ = truvari.build_anno_tree(fn)
+        self.data = pd.read_csv(fn, sep='\t', header=None)
+        self.data.columns = ['chrom', 'start', 'end', 'anno']
+        def clean_anno(x):
+            if x.startswith("ID"):
+                return x.split(';')[0][len("ID="):]
+            return x
+        self.data['anno'] = self.data['anno'].apply(clean_anno)
+    
+    def get_annotation(self, region):
+        """
+        Returns the data from annotations that the region.
+        Will assert that there aren't multiple overlaps.
+        Returns '.' if there are no annotations
+        """
+        hits = self.tree[reg['chrom']].overlap(reg['start'], reg['end'])
+        if not hits:
+            return '.'
+
+        if len(hits) == 1:
+            return self.data.iloc[list(hits)[0].data]['anno']
+        sys.stderr.write("Multianno\n%s  -  %s\n" % (reg['chrom'], str(hits)))
+        ret = []
+        for i in hits:
+            ret.append(self.data.iloc[i.data]['anno'])
+        return ",".join(ret)
+
 if __name__ == '__main__':
-    in_anno, in_ref = sys.argv[1:]
+    if len(sys.argv) == 1:
+        try:
+            for reg in iter_tr_regions("/dev/stdin"):
+                viz_region(reg)
+        except Exception:
+            for reg in iter_tr_regions_1("/dev/stdin"):
+                viz_region(reg)
+        sys.exit(0)
+    in_anno, in_ref, in_rep, patho, codis = sys.argv[1:]
+
     reference = pysam.FastaFile(in_ref)
+    repeats = joblib.load(in_rep)
+
+    patho = AnnoTree(patho)
+    codis = AnnoTree(codis)
+
     removed = 0
     total = 0
     for reg in iter_tr_regions("adotto_TRannotations_v0.3.bed.gz"):
@@ -296,9 +369,15 @@ if __name__ == '__main__':
         if len(out_annos) == 0:
             removed += 1
             continue
+
         reg['ovl_flag'] = 0
         for i in out_annos:
             reg['ovl_flag'] |= i['ovl_flag']
+        
+        reg['interspersed'] = get_interspersed(reg, repeats)
+        reg['patho'] = patho.get_annotation(reg)
+        reg['codis'] = codis.get_annotation(reg)
+
         #print(reg)
         orig_start, orig_end = reg['start'], reg['end']
         first_start, last_end = get_bounds(out_annos)
